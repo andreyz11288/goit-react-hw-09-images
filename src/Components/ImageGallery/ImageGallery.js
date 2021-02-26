deaf7c151138ce5c0b5c1aad12a98541258a8146
andreyz11288 <andreyz11288@gmail.com>
2021-02-26 15:12:43 +0200

    1

--- src/Components/ImageGallery/ImageGallery.js
+++ src/Components/ImageGallery/ImageGallery.js
@@ -1,63 +1,57 @@
 import s from './ImageGallery.module.css';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import Button from '../Button/Button';
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class ImageGallery extends Component {
-  static propTypes = {
-    onFetch: PropTypes.string,
-    src: PropTypes.func,
+const ImageGallery = ({ src, onFetch }) => {
+  const [visible, setVisible] = useState(false);
+  const [page, setPage] = useState(1);
+
+  const largeImageURL = e => {
+    src(e);
   };
 
-  state = {
-    visible: false,
-    page: 1,
-  };
-
-  largeImageURL = e => {
-    this.props.src(e);
-  };
-
-  visible = length => {
+  const visibl = length => {
     if (length) {
-      if (this.state.visible) {
-        this.setState({ visible: false });
+      if (visible) {
+        setVisible(false);
       }
     }
     if (!length) {
-      this.setState({ visible: true });
+      setVisible(true);
     }
   };
 
-  pageNumber = e => {
-    const { page } = this.state;
-    this.setState({
-      page: page + 1,
-    });
+  const pageNumber = e => {
+    setPage(page + 1);
   };
 
-  resPage = e => {
+  const resPage = e => {
     if (e) {
-      this.setState({ page: 1 });
+      setPage(1);
     }
   };
 
-  render() {
-    return (
-      <div>
-        <ul className={s.ImageGallery}>
-          <ImageGalleryItem
-            onFetch={this.props.onFetch}
-            largeImageURL={this.largeImageURL}
-            visible={this.visible}
-            numberPage={this.state.page}
-            resPage={this.resPage}
-          />
-        </ul>
-        {this.state.visible && <Button pageNumber={this.pageNumber} />}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <ul className={s.ImageGallery}>
+        <ImageGalleryItem
+          onFetch={onFetch}
+          largeImageURL={largeImageURL}
+          visible={visibl}
+          numberPage={page}
+          resPage={resPage}
+        />
+      </ul>
+      {visible && <Button pageNumber={pageNumber} />}
+    </div>
+  );
+};
+
+ImageGallery.propTypes = {
+  onFetch: PropTypes.string,
+  src: PropTypes.func,
+};
+
 export default ImageGallery;
--- src/Components/ImageGalleryItem/ImageGalleryItem.js
+++ src/Components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,113 +1,177 @@
-import { Component } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import s from './ImageGalleryItem.module.css';
 import Loaders from '../Loader/Loader';
 import Api from '../Api/Api';
 import PropTypes from 'prop-types';
 
-export default class ImageGalleryItem extends Component {
-  static propTypes = {
-    visible: PropTypes.func.isRequired,
-    resPage: PropTypes.func.isRequired,
-    largeImageURL: PropTypes.func.isRequired,
-    numberPage: PropTypes.number.isRequired,
-    onFetch: PropTypes.string,
-  };
+export default function ImageGalleryItem({
+  onFetch,
+  visible,
+  numberPage,
+  resPage,
+  largeImageURL,
+}) {
+  const [gallery, setGallery] = useState([]);
+  // const [error, setError] = useState()
+  const [status, setStatus] = useState('idle');
 
-  state = {
-    gallery: [],
-    error: null,
-    status: 'idle',
-  };
+  // state = {
+  //   gallery: [],
+  //   error: null,
+  //   status: 'idle',
+  // };
 
-  componentDidUpdate(prevProps, prevState) {
+  const prevStateStatus = useRef(status);
+  const prevPropsOnFetch = useRef(onFetch);
+  const prevPropsNumberPage = useRef(numberPage);
+  useEffect(() => {
     let thisPage = 1;
 
-    if (this.props.onFetch === '' && prevState.status !== this.state.status) {
-      this.setState({ status: 'idle' });
-      this.props.visible(true);
+    if (onFetch === '' && prevStateStatus.current !== status) {
+      console.log('1');
+      setStatus('idle');
+      visible(true);
     }
     if (
-      prevProps.onFetch !== this.props.onFetch &&
-      prevProps.numberPage === this.props.numberPage
+      prevPropsOnFetch.current !== onFetch &&
+      prevPropsNumberPage.current === numberPage
     ) {
-      this.setState({ gallery: [] });
-      this.setState({ status: 'pending' });
-      this.props.visible(true);
-      this.props.resPage(true);
+      console.log('2');
+      setGallery([]);
+      // setStatus( 'pending' );
+      visible(true);
+      resPage(true);
 
-      Api(this.props.onFetch, thisPage)
+      Api(onFetch, thisPage)
         .then(e => {
-          this.setState({ gallery: e.hits });
-          this.props.visible(false);
+          setGallery(e.hits);
+          visible(false);
           if (e.hits.length === 0) {
-            this.props.visible(true);
-            return this.setState({ status: 'rejected' });
+            visible(true);
+            return setStatus('rejected');
           }
-          this.setState({ status: 'resolve' });
+          setStatus('resolve');
         })
-        .catch(() => this.setState({ status: 'rejected' }));
+        .catch(() => setStatus('rejected'));
     }
     if (
-      prevProps.onFetch === this.props.onFetch &&
-      prevProps.numberPage < this.props.numberPage
+      prevPropsOnFetch.current === onFetch &&
+      prevPropsNumberPage.current < numberPage
     ) {
-      thisPage = this.props.numberPage;
-      Api(this.props.onFetch, thisPage)
+      console.log('3');
+      thisPage = numberPage;
+      Api(onFetch, thisPage)
         .then(e => {
-          const { gallery } = this.state;
-          this.setState({ gallery: [...gallery, ...e.hits] });
+          // const { gallery } = this.state;
+          setGallery([...gallery, ...e.hits]);
           window.scrollTo({
             top: document.documentElement.scrollHeight,
             behavior: 'smooth',
           });
-          this.props.visible(false);
+          visible(false);
           if (e.hits.length === 0) {
-            this.props.visible(true);
+            visible(true);
           }
-          this.setState({ status: 'resolve' });
+          setStatus('resolve');
         })
-        .catch(() => this.setState({ status: 'rejected' }));
+        .catch(() => setStatus('rejected'));
     }
+  }, [gallery, numberPage, onFetch, resPage, status, visible]);
+
+  // const componentDidUpdate(prevProps, prevState) {
+  // let thisPage = 1;
+
+  // if (this.props.onFetch === '' && prevState.status !== this.state.status) {
+  //   this.setState({ status: 'idle' });
+  //   this.props.visible(true);
+  // }
+  // if (
+  //   prevProps.onFetch !== this.props.onFetch &&
+  //   prevProps.numberPage === this.props.numberPage
+  // ) {
+  //   this.setState({ gallery: [] });
+  //   this.setState({ status: 'pending' });
+  //   this.props.visible(true);
+  //   this.props.resPage(true);
+
+  //   Api(this.props.onFetch, thisPage)
+  //     .then(e => {
+  //       this.setState({ gallery: e.hits });
+  //       this.props.visible(false);
+  //       if (e.hits.length === 0) {
+  //         this.props.visible(true);
+  //         return this.setState({ status: 'rejected' });
+  //       }
+  //       this.setState({ status: 'resolve' });
+  //     })
+  //     .catch(() => this.setState({ status: 'rejected' }));
+  // }
+  // if (
+  //   prevProps.onFetch === this.props.onFetch &&
+  //   prevProps.numberPage < this.props.numberPage
+  // ) {
+  //   thisPage = this.props.numberPage;
+  //   Api(this.props.onFetch, thisPage)
+  //     .then(e => {
+  //       const { gallery } = this.state;
+  //       this.setState({ gallery: [...gallery, ...e.hits] });
+  //       window.scrollTo({
+  //         top: document.documentElement.scrollHeight,
+  //         behavior: 'smooth',
+  //       });
+  //       this.props.visible(false);
+  //       if (e.hits.length === 0) {
+  //         this.props.visible(true);
+  //       }
+  //       this.setState({ status: 'resolve' });
+  //     })
+  //     .catch(() => this.setState({ status: 'rejected' }));
+  // }
+  // }
+
+  // const { status, gallery } = this.state;
+
+  if (status === 'idle') {
+    return (
+      <li>
+        <h1>Введите запрос</h1>
+      </li>
+    );
   }
 
-  render() {
-    const { status, gallery } = this.state;
+  if (status === 'rejected') {
+    return (
+      <li>
+        <h1>Запрос не найден</h1>
+      </li>
+    );
+  }
+  if (status === 'pending') {
+    return (
+      <li>
+        <Loaders />
+      </li>
+    );
+  }
 
-    if (status === 'idle') {
-      return (
-        <li>
-          <h1>Введите запрос</h1>
-        </li>
-      );
-    }
-
-    if (status === 'rejected') {
-      return (
-        <li>
-          <h1>Запрос не найден</h1>
-        </li>
-      );
-    }
-    if (status === 'pending') {
-      return (
-        <li>
-          <Loaders />
-        </li>
-      );
-    }
-
-    if (status === 'resolve') {
-      return gallery.map(e => (
-        <li key={e.id} className={s.ImageGalleryItem}>
-          <img
-            src={e.webformatURL}
-            data-src={e.largeImageURL}
-            alt="img"
-            className={s.ImageGalleryItemImage}
-            onClick={() => this.props.largeImageURL(e.largeImageURL)}
-          />
-        </li>
-      ));
-    }
+  if (status === 'resolve') {
+    return gallery.map(e => (
+      <li key={e.id} className={s.ImageGalleryItem}>
+        <img
+          src={e.webformatURL}
+          data-src={e.largeImageURL}
+          alt="img"
+          className={s.ImageGalleryItemImage}
+          onClick={() => largeImageURL(e.largeImageURL)}
+        />
+      </li>
+    ));
   }
 }
+ImageGalleryItem.propTypes = {
+  visible: PropTypes.func.isRequired,
+  resPage: PropTypes.func.isRequired,
+  largeImageURL: PropTypes.func.isRequired,
+  numberPage: PropTypes.number.isRequired,
+  onFetch: PropTypes.string,
+};
